15770d942fb99f7b91c06a7ff158d1cf8ee606d9
TheRealT-Dawg <Johnbabbel2@gmail.com>
2026-06-07 18:15:59 -0500

    Renamed the syntax folder to Basics + first program
    Renamed the syntax folder because I made a file with the same name so i couldn't commit changes and created program to get commander name + rocket destination and show the two.

---
+++ Basics/day_one_logic.py
@@ -0,0 +1,14 @@
+# Project: Rocket Command Line Interface
+# Right now just practicing with variables, 'print' and 'input'
+
+print("-------- COMMAND LAUNCH TERMINAL VIA GT--------")
+
+# 1. Gathering the data and assigning them to variables
+    commander_name = input("Enter Astronaut Name")
+    rocket_destination = input("Enter Target Destination")
+     
+    print("-----Generating Flight Manifest-----")
+
+#2. Using print and the variables to actually show the data
+    print("Commander on Deck " + commander_name)
+    print("Target Trajectory: " + rocket_destination)
